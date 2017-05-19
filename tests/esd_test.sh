6e72c28ad467705cd6fdbfe4d5e753f45732163f
Aravinth Panchadcharam <panch.aravinth@gmail.com>
2017-05-19 17:45:52 +0200

    Update ESD test script with LED status

--- tests/esd_test.sh
+++ tests/esd_test.sh
@@ -1,21 +1,45 @@
-#!/bin/sh
+#!/bin/bash
 ### BEGIN INIT INFO
 # Provides: stress_test
 # Required-Start:
 # Required-Stop:
 # Default-Start:     S
 # Default-Stop:
-# Short-Description: Test suit to test survival on ESD 
+# Short-Description: Test suite to test survival on ESD / EMV Test 
 ### END INIT INFO
 
-echo -e "\n [Senic Hub] ESD survival test"
-IS_WLAN="$(usb-devices | grep mt7601u | wc -l)"
-IS_BLE="$(usb-devices | grep btusb | wc -l)"
+echo -e "[Senic Hub] ESD survival test"
+echo none > /sys/class/leds/nanopi:blue:status/trigger
+echo 0 > /sys/class/leds/nanopi:blue:status/brightness
 
+USB_LIST="$(usb-devices)"
+IS_WLAN="$(echo "$USB_LIST" | grep mt7601u | wc -l)"
+IS_BLE="$(echo "$USB_LIST" | grep btusb | wc -l)"
+
+switch_led_on()
+{
+	echo 255 > /sys/class/leds/nanopi:blue:status/brightness
+}
+
+switch_led_off()
+{
+	echo 0 > /sys/class/leds/nanopi:blue:status/brightness
+}
+
+# Check if WiFi dongle is found
 if [ $IS_WLAN -gt 0 ]; then
 	echo " WLAN Dongle is found"
+	switch_led_on
+else
+	echo " WLAN Dongle is not found"
+	switch_led_off
 fi
 
+# Check if Bluetooth dongle is found
 if [ $IS_BLE -gt 0 ]; then
 	echo " BLE Dongle is found"
+	switch_led_on
+else 
+	echo " BLE Dongle is not found"
+	switch_led_off
 fi
