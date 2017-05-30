fdc686cf5305bce7aefe055bfff4904645983af9
Aravinth Panchadcharam <panch.aravinth@gmail.com>
2017-05-30 17:12:14 +0200

    Pull down to switch LED on in ESD Test

--- meta-senic/recipes-core/initscripts/initscripts/esd_test.sh
+++ meta-senic/recipes-core/initscripts/initscripts/esd_test.sh
@@ -22,29 +22,34 @@ IS_BLE="$(echo "$USB_LIST" | grep btusb | wc -l)"
 switch_led_on()
 {
 	echo 255 > /sys/class/leds/nanopi:blue:status/brightness
-	echo 1 > /sys/class/gpio/gpio198/value
+	echo 0 > /sys/class/gpio/gpio198/value
 }
 
 switch_led_off()
 {
 	echo 0 > /sys/class/leds/nanopi:blue:status/brightness
-	echo 0 > /sys/class/gpio/gpio198/value
+	echo 1 > /sys/class/gpio/gpio198/value
 }
 
 # Check if WiFi dongle is found
 if [ $IS_WLAN -gt 0 ]; then
-	echo " WLAN Dongle is found"
-	switch_led_on
+	echo " WLAN Dongle is found"	
 else
-	echo " WLAN Dongle is not found"
-	switch_led_off
+	echo " WLAN Dongle is not found"	
 fi
 
 # Check if Bluetooth dongle is found
 if [ $IS_BLE -gt 0 ]; then
-	echo " BLE Dongle is found"
-	switch_led_on
+	echo " BLE Dongle is found"	
 else 
-	echo " BLE Dongle is not found"
+	echo " BLE Dongle is not found"	
+fi
+
+# Switch LED on if tests passed
+if [ $IS_WLAN -gt 0 ] && [ $IS_BLE -gt 0 ]; then
+	echo " TEST passed"	
+	switch_led_on
+else
+	echo " TEST failed"	
 	switch_led_off
 fi
