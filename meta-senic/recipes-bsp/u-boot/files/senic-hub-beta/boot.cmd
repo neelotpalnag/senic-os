59f78869dae947e9d0550270e5e344a55df7bb9e
Mylène Josserand <mylene.josserand@free-electrons.com>
2017-08-08 11:08:11 +0200

    u-boot: Fix boot script for senic-hub-beta image
    In case of senic-hub-beta image, I forgot to add "boot"
    command in the boot script. Because of that, it is just setting
    environment and so, the script will fail:
    	reading /boot.scr
    	417 bytes read in 15 ms (26.4 KiB/s)
    	## Executing script at 43100000
    	SCRIPT FAILED: continuing...
    
    Adding "boot" command will use the previous bootcmd and
    not the default one (which does not use mender's variables).
    
    Signed-off-by: Mylène Josserand <mylene.josserand@free-electrons.com>

--- meta-senic/recipes-bsp/u-boot/files/senic-hub-beta/boot.cmd
+++ meta-senic/recipes-bsp/u-boot/files/senic-hub-beta/boot.cmd
@@ -2,3 +2,4 @@ setenv fdtfile boot/sun8i-h3-nanopi-m1.dtb
 setenv bootargs 'console=${console} root=${mender_kernel_root} rootwait panic=10 ${extra}'
 setenv mmcboot 'load ${mender_uboot_root} ${fdt_addr_r} ${fdtfile}; load ${mender_uboot_root} ${kernel_addr_r} boot/zImage; bootz ${kernel_addr_r} - ${fdt_addr_r}'
 setenv bootcmd 'run mender_setup; run mmcboot'
+boot
